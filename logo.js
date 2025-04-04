699dc32e1a5c3f4af9da31233477d192ec61fbe5
Ayush Kanoje <ayushkanoje056@gmail.com>
2025-04-04 13:17:50 +0530

    Adding all intial files

---
+++ logo.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function(){
+    setTimeout(() => {
+         document.querySelector(".logo-c").style.animation = "moveLogo 3s ease-in-out forwards";
+    
+    
+    }, 2000);
+
+});
